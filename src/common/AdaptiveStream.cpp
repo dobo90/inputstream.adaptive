c5c40a5d0f744fdfe4c028de7a52a55e5c3b8f77
Dobroslaw Kijowski <dobo90@gmail.com>
2025-05-05 18:53:24 +0200

    Fix memory leak in AdaptiveStream::parseIndexRange
    * Leak can be reproduced by playing 'Google tears [default_KID on common CP]'
      from plugin.video.isasamples.
    
    Direct leak of 88 byte(s) in 1 object(s) allocated from:
        #0 0x78b3c6efe392 in operator new(unsigned long) /usr/src/debug/gcc/gcc/libsanitizer/asan/asan_new_delete.cpp:95
        #1 0x78b37a774b7e in AP4_AtomFactory::CreateAtomFromStream(AP4_ByteStream&, unsigned long long&, AP4_Atom*&) /home/dobo/kodi/inputstream.adaptive/bento4/src/bento4/Source/C++/Core/Ap4AtomFactory.cp
    p:242
        #2 0x78b37a77487d in AP4_AtomFactory::CreateAtomFromStream(AP4_ByteStream&, AP4_Atom*&) /home/dobo/kodi/inputstream.adaptive/bento4/src/bento4/Source/C++/Core/Ap4AtomFactory.cpp:154
        #3 0x78b37a5b55ce in adaptive::AdaptiveStream::parseIndexRange(PLAYLIST::CRepresentation*, std::vector<unsigned char, std::allocator<unsigned char> > const&) /home/dobo/kodi/inputstream.adaptive/sr
    c/common/AdaptiveStream.cpp:508
        #4 0x78b37a5bd0c5 in adaptive::AdaptiveStream::GenerateSidxSegments(PLAYLIST::CRepresentation*) /home/dobo/kodi/inputstream.adaptive/src/common/AdaptiveStream.cpp:1343
        #5 0x78b37a5b620d in adaptive::AdaptiveStream::start_stream(unsigned long) /home/dobo/kodi/inputstream.adaptive/src/common/AdaptiveStream.cpp:650
        #6 0x78b37a502434 in CInputStreamAdaptive::OpenStream(int) /home/dobo/kodi/inputstream.adaptive/src/main.cpp:254
        #7 0x78b37a50e1fd in kodi::addon::CInstanceInputStream::ADDON_OpenStream(AddonInstance_InputStream const*, int) (/usr/lib/kodi/addons/inputstream.adaptive/inputstream.adaptive.so.21.5.9+0xb0e1fd) (
    BuildId: f190da3ba02e0d5058c85a1816f30382ce0a2e08)
        #8 0x63de0ca162b5 in CDVDDemuxClient::OpenStream(int) (/usr/lib/kodi/kodi.bin+0xb972b5) (BuildId: 23d1103e1367c2f49a1740c7881fb1656fe68f51)
        #9 0x63de0ca94129 in CVideoPlayer::OpenStream(CCurrentStream&, long, int, int, bool) (/usr/lib/kodi/kodi.bin+0xc15129) (BuildId: 23d1103e1367c2f49a1740c7881fb1656fe68f51)
        #10 0x63de0ca948c2 in CVideoPlayer::OpenDefaultStreams(bool) (/usr/lib/kodi/kodi.bin+0xc158c2) (BuildId: 23d1103e1367c2f49a1740c7881fb1656fe68f51)
        #11 0x63de0ca9a93f in CVideoPlayer::Prepare() (/usr/lib/kodi/kodi.bin+0xc1b93f) (BuildId: 23d1103e1367c2f49a1740c7881fb1656fe68f51)
        #12 0x63de0ca9d1d3 in CVideoPlayer::Process() (/usr/lib/kodi/kodi.bin+0xc1e1d3) (BuildId: 23d1103e1367c2f49a1740c7881fb1656fe68f51)
        #13 0x63de0ccff85e in CThread::Action() (/usr/lib/kodi/kodi.bin+0xe8085e) (BuildId: 23d1103e1367c2f49a1740c7881fb1656fe68f51)
        #14 0x63de0cd081d4  (/usr/lib/kodi/kodi.bin+0xe891d4) (BuildId: 23d1103e1367c2f49a1740c7881fb1656fe68f51)
        #15 0x78b3c46e1f73 in execute_native_thread_routine /usr/src/debug/gcc/gcc/libstdc++-v3/src/c++11/thread.cc:104
        #16 0x78b3c6e5cec9 in asan_thread_start /usr/src/debug/gcc/gcc/libsanitizer/asan/asan_interceptors.cpp:234
        #17 0x78b3c44a3707  (/usr/lib/libc.so.6+0x95707) (BuildId: d056ce83eebe65ce7e52ecfa5af5363e4863d283)

--- src/common/AdaptiveStream.cpp
+++ src/common/AdaptiveStream.cpp
@@ -564,6 +564,10 @@ bool adaptive::AdaptiveStream::parseIndexRange(PLAYLIST::CRepresentation* rep,
         sidxCount--;
         delete atom;
       }
+      else
+      {
+        delete atom;
+      }
     }
 
     if (!rep->HasInitSegment())
