135a8e8a46be924c03222c621e26516e3987b741
Dobroslaw Kijowski <dobo90@gmail.com>
2025-05-09 12:34:59 +0200

    Drop secure decoder and remove all decrypters
    * Code compiles and ISA is able to play not encrypted media.
    * Intentionally leave configuration to keep this fork interchangable with upstream.

--- src/decrypters/DrmEngine.h
+++ src/decrypters/DrmEngine.h
@@ -40,47 +40,19 @@ public:
   virtual ~CDRMEngine() = default;
 
   /*!
-   * \brief Pre-initialize a DRM before to download a manifest and create a session.
-   * \param session[OUT] The opened session
-   * \return True if has been pre-initialized, otherwise false
-   */
-  bool PreInitializeDRM(DRMSession& session);
-
-  /*!
    * \brief Initialize a DRM (if needed), then create or reuse a session.
    * \param drmInfos The DRM info provided by a manifest
-   * \param mediaType The type of media involved in the session
-   * \param isForceSecureDecoder[OPT] To force the Secure Decoder, this setting coming from the manifest
    * \param streamInfo The InputstreamInfo where set the DRM configuration
    * \param repr The representation of the stream refer to drmInfos
    * \param adp The adaptation set of the representation
-   * \param canCleanupSessions Delete existing sessions before to create a new one
    * \param initDrmInfo[OUT] Set the DRMInfo used to initialize, otherwise the value is unchanged
    * \return True if has success, otherwise false
    */
   bool InitializeSession(std::vector<DRM::DRMInfo> drmInfos,
-                         DRM::DRMMediaType mediaType,
-                         std::optional<bool> isForceSecureDecoder,
                          kodi::addon::InputstreamInfo& streamInfo,
                          PLAYLIST::CRepresentation* repr,
                          PLAYLIST::CAdaptationSet* adp,
-                         bool canCleanupSessions,
                          DRM::DRMInfo& initDrmInfo);
-
-  /*!
-   * \brief Get the session by ID (dont take in account KID).
-   * \param id The session ID
-   * \return The session, otherwise nullptr if not found
-   */
-  const DRMSession* GetSession(const std::string& id) const;
-
-  /*!
-   * \brief Get the session by ID that match the specified KID.
-   * \param id The session ID
-   * \return The session, otherwise nullptr if not found
-   */
-  const DRMSession* GetSession(const std::string& id, const std::string& kid) const;
-
 
   /*!
    * \brief Get the current engine status. The state can change after each call to InitializeSession.
@@ -115,9 +87,6 @@ private:
                                    PLAYLIST::CAdaptationSet* adp,
                                    DRM::DRMInfo& drmInfo);
 
-  // \brief Delete all sessions by media type
-  void DeleteSessionsByType(const DRMMediaType type);
-
   // \brief Check if a Key System is supported
   bool HasKeySystemSupport(std::string_view keySystem) const;
 
@@ -126,10 +95,8 @@ private:
 
   std::string m_keySystem; // Choosen key system
   std::vector<DRMInstance> m_drms;
-  std::vector<DRMSession> m_sessions;
 
   EngineStatus m_status{EngineStatus::NONE};
-  bool m_isPreinitialized{false};
 };
 
 } // namespace DRM
--- src/samplereader/FragmentedSampleReader.h
+++ src/samplereader/FragmentedSampleReader.h
@@ -18,7 +18,6 @@
 #include <mutex>
 
 // forwards
-class CAdaptiveCencSampleDecrypter;
 class CodecHandler;
 
 // \brief Redirects AP4_LinearReader callbacks
@@ -96,9 +95,7 @@ public:
   Type GetType() const override { return Type::FMP4; }
 
   virtual bool Initialize(SESSION::CStream* stream) override;
-  virtual void SetDecrypter(std::shared_ptr<Adaptive_CencSingleSampleDecrypter> ssd,
-                            const DRM::DecrypterCapabilites& dcaps,
-                            const std::vector<uint8_t>& defaultKid) override;
+  virtual std::vector<uint8_t> SetDefaultKid(const std::vector<uint8_t>& defaultKid) override;
 
   AP4_Result Start(std::optional<uint64_t> pts) override;
   AP4_Result ReadSample() override;
@@ -110,14 +107,12 @@ public:
   AP4_Size GetSampleDataSize() const override { return m_sampleData.GetDataSize(); }
   const AP4_Byte* GetSampleData() const override { return m_sampleData.GetData(); }
   uint64_t GetDuration() const override;
-  bool IsEncrypted() const override;
   bool GetInformation(kodi::addon::InputstreamInfo& info) override;
   bool TimeSeek(uint64_t pts) override;
   void SetPTSOffset(uint64_t offset) override;
   int64_t GetPTSDiff() const override { return m_ptsDiff; }
   bool GetFragmentInfo(uint64_t& duration) override;
   uint32_t GetTimeScale() const override { return m_track->GetMediaTimeScale(); }
-  CryptoInfo GetReaderCryptoInfo() const override { return m_readerCryptoInfo; }
 
   /*!
    * \brief Create a new reader by sharing the same data reader and segment management (AdaptiveStream).
@@ -141,8 +136,6 @@ private:
   AP4_Track* m_track;
   AP4_UI32 m_poolId{0};
   AP4_UI32 m_sampleDescIndex{1};
-  DRM::DecrypterCapabilites m_decrypterCaps;
-  unsigned int m_failCount{0};
   bool m_bSampleDescChanged{false};
   bool m_eos{false};
   bool m_started{false};
@@ -157,9 +150,7 @@ private:
   CodecHandler* m_codecHandler{nullptr};
   std::vector<uint8_t> m_defaultKey;
   AP4_ProtectedSampleDescription* m_protectedDesc{nullptr};
-  std::shared_ptr<Adaptive_CencSingleSampleDecrypter> m_singleSampleDecryptor{nullptr};
-  std::unique_ptr<CAdaptiveCencSampleDecrypter> m_decrypter;
-  CryptoInfo m_readerCryptoInfo{};
+  std::shared_ptr<AP4_CencSampleDecrypter> m_decrypter;
 
   std::shared_ptr<CLinearReader> m_lReader;
 };
