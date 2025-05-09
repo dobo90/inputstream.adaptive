cf68ea30ce5a77c277b6d91aeb838ff47ac19f71
Dobroslaw Kijowski <dobo90@gmail.com>
2025-05-09 12:34:59 +0200

    Drop secure decoder and remove all decrypters
    * Code compiles and ISA is able to play not encrypted media.
    * Intentionally leave configuration to keep this fork interchangable with upstream.

--- src/parser/SmoothTree.cpp
+++ src/parser/SmoothTree.cpp
@@ -122,8 +122,6 @@ bool adaptive::CSmoothTree::ParseManifest(const std::string& data)
   xml_node nodeProt = nodeSSM.child("Protection");
   if (nodeProt)
   {
-    period->SetSecureDecodeNeeded(true);
-
     for (xml_node nodePH : nodeProt.children("ProtectionHeader"))
     {
       // SystemID can be wrapped by {}
@@ -137,12 +135,6 @@ bool adaptive::CSmoothTree::ParseManifest(const std::string& data)
           drmInfo.licenseServerUri = protParser.GetLicenseURL();
           drmInfo.initData = DRM::PSSH::Make(DRM::ID_PLAYREADY, {}, protParser.GetInitData());
           drmInfo.defaultKid = STRING::ToLower(STRING::ToHexadecimal(protParser.GetKID()));
-
-          auto encryptionType = protParser.GetEncryption();
-          if (encryptionType == DRM::PRHeaderParser::EncryptionType::AESCTR)
-            drmInfo.cryptoMode = CryptoMode::AES_CTR;
-          else if (encryptionType == DRM::PRHeaderParser::EncryptionType::AESCBC)
-            drmInfo.cryptoMode = CryptoMode::AES_CBC;
 
           drmInfos.emplace_back(drmInfo);
         }
