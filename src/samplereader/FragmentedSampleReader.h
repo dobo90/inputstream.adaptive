8455a7fd7d0c5979fb82cfbecd0036f7366948af
Dobroslaw Kijowski <dobo90@gmail.com>
2025-05-09 12:34:59 +0200

    Drop secure decoder and remove all decrypters
    * Code compiles and ISA is able to play not encrypted media.
    * Intentionally leave configuration to keep this fork interchangable with upstream.

--- src/samplereader/FragmentedSampleReader.h
+++ src/samplereader/FragmentedSampleReader.h
@@ -12,7 +12,6 @@
 #include "decrypters/IDecrypter.h"
 
 // forwards
-class CAdaptiveCencSampleDecrypter;
 class CodecHandler;
 
 class ATTR_DLL_LOCAL CFragmentedSampleReader : public ISampleReader, public AP4_LinearReader
@@ -23,8 +22,6 @@ public:
   ~CFragmentedSampleReader();
 
   virtual bool Initialize(SESSION::CStream* stream) override;
-  virtual void SetDecrypter(std::shared_ptr<Adaptive_CencSingleSampleDecrypter> ssd,
-                            const DRM::DecrypterCapabilites& dcaps) override;
 
   AP4_Result Start(bool& bStarted) override;
   AP4_Result ReadSample() override;
@@ -36,14 +33,12 @@ public:
   AP4_Size GetSampleDataSize() const override { return m_sampleData.GetDataSize(); }
   const AP4_Byte* GetSampleData() const override { return m_sampleData.GetData(); }
   uint64_t GetDuration() const override;
-  bool IsEncrypted() const override;
   bool GetInformation(kodi::addon::InputstreamInfo& info) override;
   bool TimeSeek(uint64_t pts, bool preceeding) override;
   void SetPTSOffset(uint64_t offset) override;
   int64_t GetPTSDiff() const override { return m_ptsDiff; }
   bool GetFragmentInfo(uint64_t& duration) override;
   uint32_t GetTimeScale() const override { return m_track->GetMediaTimeScale(); }
-  CryptoInfo GetReaderCryptoInfo() const override { return m_readerCryptoInfo; }
 
 protected:
   AP4_Result ProcessMoof(AP4_ContainerAtom* moof,
@@ -58,8 +53,6 @@ private:
   AP4_Track* m_track;
   AP4_UI32 m_poolId{0};
   AP4_UI32 m_sampleDescIndex{1};
-  DRM::DecrypterCapabilites m_decrypterCaps;
-  unsigned int m_failCount{0};
   bool m_bSampleDescChanged{false};
   bool m_eos{false};
   bool m_started{false};
@@ -74,7 +67,5 @@ private:
   CodecHandler* m_codecHandler{nullptr};
   std::vector<uint8_t> m_defaultKey;
   AP4_ProtectedSampleDescription* m_protectedDesc{nullptr};
-  std::shared_ptr<Adaptive_CencSingleSampleDecrypter> m_singleSampleDecryptor{nullptr};
-  std::unique_ptr<CAdaptiveCencSampleDecrypter> m_decrypter;
-  CryptoInfo m_readerCryptoInfo{};
+  std::unique_ptr<AP4_CencSampleDecrypter> m_decrypter;
 };
