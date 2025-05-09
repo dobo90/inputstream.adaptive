f9dd22d54e3f2728e9c260ff18626d22313eed8e
Dobroslaw Kijowski <dobo90@gmail.com>
2025-05-09 12:34:59 +0200

    Drop secure decoder and remove all decrypters
    * Code compiles and ISA is able to play not encrypted media.
    * Intentionally leave configuration to keep this fork interchangable with upstream.

--- src/decrypters/HelperPr.h
+++ src/decrypters/HelperPr.h
@@ -20,13 +20,6 @@ namespace DRM
 class PRHeaderParser
 {
 public:
-  enum class EncryptionType
-  {
-    UNKNOWN,
-    AESCTR, // cenc
-    AESCBC, // cbcs
-  };
-
   /*!
    * \brief Parse PlayReady header data.
    * \param prHeader The PlayReady header data as base64 string
@@ -46,13 +39,11 @@ public:
    * \brief Get keyid as 16 bytes format (converted for Widevine DRM)
    */
   const std::vector<uint8_t>& GetKID() const { return m_KID; }
-  EncryptionType GetEncryption() const { return m_encryption; }
   std::string_view GetLicenseURL() const { return m_licenseURL; }
   const std::vector<uint8_t>& GetInitData() const { return m_initData; }
 
 private:
   std::vector<uint8_t> m_KID;
-  EncryptionType m_encryption{EncryptionType::UNKNOWN};
   std::string m_licenseURL;
   std::vector<uint8_t> m_initData;
 };
