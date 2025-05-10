b1a9036f984d86f78106cdc366f01a546e2e7819
Dobroslaw Kijowski <dobo90@gmail.com>
2025-05-10 13:32:17 +0200

    Integrate with widevine-rs

--- src/decrypters/DrmFactory.cpp
+++ src/decrypters/DrmFactory.cpp
@@ -13,6 +13,7 @@
 #include "utils/Base64Utils.h"
 #include "utils/StringUtils.h"
 #include "utils/log.h"
+#include "widevine/WidevineCdm.h"
 
 #include <kodi/addon-instance/inputstream/StreamCrypto.h>
 
@@ -93,5 +94,10 @@ DRM::Config DRM::CreateDRMConfig(std::string_view keySystem, const ADP::KODI_PRO
 
 std::shared_ptr<DRM::Cdm> DRM::FACTORY::GetCdm(STREAM_CRYPTO_KEY_SYSTEM keySystem)
 {
+  if (keySystem == STREAM_CRYPTO_KEY_SYSTEM_WIDEVINE)
+  {
+    return std::shared_ptr<DRM::Cdm>(new WidevineCdm());
+  }
+
   return nullptr;
 }
