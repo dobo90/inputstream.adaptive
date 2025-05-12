f51a4210e229a2478f9ea2404c4f1e892ac827f5
Dobroslaw Kijowski <dobo90@gmail.com>
2025-05-12 20:00:47 +0200

    Add minimalistic ClearKey implementation
    * Accepts keys set only in properties
    * It doesn't implement getting keys from remote server

---
+++ src/decrypters/clearkey/ClearKeyCdm.cpp
@@ -0,0 +1,41 @@
+#include "ClearKeyCdm.h"
+
+#include "utils/StringUtils.h"
+
+using namespace UTILS;
+
+SResult ClearKeyCdm::OpenDRMSystem(const DRM::Config& config)
+{
+  m_isInitialised = true;
+  m_config = config;
+  return SResultCode::OK;
+}
+
+bool ClearKeyCdm::GetKeysFromLicenseServer(const std::vector<uint8_t>& pssh,
+                                           const std::vector<uint8_t>& kid)
+{
+  if (GetKey(kid).has_value())
+  {
+    return true;
+  }
+
+  const auto& keys = m_config.license.keys;
+  bool foundKey = false;
+
+  for (const auto& [hexKid, hexKey] : keys)
+  {
+    std::vector<uint8_t> newKid, newKey;
+
+    STRING::ToHexBytes(hexKid, newKid);
+    STRING::ToHexBytes(hexKey, newKey);
+
+    if (kid == newKid)
+    {
+      foundKey = true;
+    }
+
+    AddKey(std::move(newKid), std::move(newKey));
+  }
+
+  return foundKey;
+}
