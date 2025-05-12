c56fc49b7c170fa913a4d8b1535e2e845ca58f0f
Dobroslaw Kijowski <dobo90@gmail.com>
2025-05-12 16:04:48 +0200

    Integrate with playready-rs

--- src/decrypters/DrmFactory.cpp
+++ src/decrypters/DrmFactory.cpp
@@ -8,6 +8,7 @@
 
 #include "DrmFactory.h"
 
+#include "playready/PlayReadyCdm.h"
 #include "widevine/WidevineCdm.h"
 
 #include <kodi/c-api/addon-instance/inputstream/stream_crypto.h>
@@ -19,6 +20,10 @@ Cdm* DRM::FACTORY::GetCdm(STREAM_CRYPTO_KEY_SYSTEM keySystem)
   if (keySystem == STREAM_CRYPTO_KEY_SYSTEM_WIDEVINE)
   {
     return new WidevineCdm();
+  }
+  else if (keySystem == STREAM_CRYPTO_KEY_SYSTEM_PLAYREADY)
+  {
+    return new PlayreadyCdm();
   }
 
   return nullptr;
