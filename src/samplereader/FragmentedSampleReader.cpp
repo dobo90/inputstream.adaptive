54490598d84e67f4ee007abb04402124e925a9c0
Dobroslaw Kijowski <dobo90@gmail.com>
2025-12-28 21:56:19 +0100

    Partially revert "[LinearReader] Unset owned sample table to avoid Advance delete it"
    This reverts commit 8d2459d5077ba7f96ac99c279c96d4dc25e80959 from PR [1].
    
    Maybe it fixes https://github.com/xbmc/inputstream.adaptive/issues/1909
    but it introduces memory leak on each seek (even on regular streams).
    
    With this revert, on included audio streams FMP4 seeking sometimes fails
    and prematurely ends playback (but at least it doesn't leak).
    
    Tested on stream [2].
    
    [1]: https://github.com/xbmc/inputstream.adaptive/pull/1967
    [2]: https://github.com/emilsvennesson/script.module.inputstreamhelper/issues/636

--- src/samplereader/FragmentedSampleReader.cpp
+++ src/samplereader/FragmentedSampleReader.cpp
@@ -953,14 +953,8 @@ AP4_Result CLinearReader::SeekSample(CLinearReaderCB* lReaderCB,
   {
     tracker->m_NextSampleIndex = tracker->m_SampleTable->GetSampleCount();
 
-    const bool isOwned = tracker->m_SampleTableIsOwned;
-    // Temporarily set m_SampleTableIsOwned to false to prevent "Advance" method
-    // from deleting the tracker sample table
-    tracker->m_SampleTableIsOwned = false;
-
     if (AP4_FAILED(result = Advance()))
     {
-      tracker->m_SampleTableIsOwned = isOwned;
       return result;
     }
     // Ensure that Advance has populated the sample table
@@ -970,7 +964,6 @@ AP4_Result CLinearReader::SeekSample(CLinearReaderCB* lReaderCB,
       return AP4_ERROR_INVALID_STATE;
     }
 
-    tracker->m_SampleTableIsOwned = isOwned;
     samplesCount = tracker->m_SampleTable->GetSampleCount();
 
     if (samplesCount == 0)
